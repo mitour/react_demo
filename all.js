6f2434ff3a944fa091f3964e1f241f732a4c5d1d
Mitour <love2082020@gmail.com>
2022-08-04 09:15:22 +0800

    re-render using setInterval

--- all.js
+++ all.js
@@ -1,6 +1,9 @@
-const CallDogName = () => "Bob";
-const content = <h1 className="royalblue">Hey {CallDogName()}</h1>;
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const Tick = () => {
+  const content = (
+    <h1 className="royalblue">It's {new Date().toLocaleTimeString()} now</h1>
+  );
+  root.render(content);
+};
 
-root.render(content);
+setInterval(Tick, 1000);
