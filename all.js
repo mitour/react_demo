a643417090c458e83e871d05050bae95a3b3e195
Mitour <love2082020@gmail.com>
2022-08-04 09:35:23 +0800

    props

--- all.js
+++ all.js
@@ -1,8 +1,11 @@
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const Welcome = () => `Hello, How are U`;
+const Welcome = (props) => {
+  const sayHiCount = 1;
+  return `Hey, I'm ${props.name}. I'm ${props.age} years old. ${sayHiCount} times say hi to you.`;
+};
 const content = (
   <h1 className="royalblue">
-    <Welcome></Welcome>
+    <Welcome name="Mary" age="15"></Welcome>
   </h1>
 );
 root.render(content);
