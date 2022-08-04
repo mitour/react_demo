e31a0417f07df9c040ba9b1b34c0db914e7ced0a
Mitour <love2082020@gmail.com>
2022-08-04 09:23:23 +0800

    component

--- all.js
+++ all.js
@@ -1,9 +1,8 @@
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const Tick = () => {
-  const content = (
-    <h1 className="royalblue">It's {new Date().toLocaleTimeString()} now</h1>
-  );
-  root.render(content);
-};
-
-setInterval(Tick, 1000);
+const Welcome = () => `Hello, How are U`;
+const content = (
+  <h1 className="royalblue">
+    <Welcome></Welcome>
+  </h1>
+);
+root.render(content);
