820d02bd89449dad478c34638b1493372ed388a7
Mitour <love2082020@gmail.com>
2022-08-11 14:51:23 +0800

    more exercise about pass props through components II

--- component.js
+++ component.js
@@ -2,6 +2,11 @@ const { useState } = React;
 
 function AddMenuInput({ item, setItem }) {
   const [input, setInput] = useState("");
+  const handleClick = () => {
+    if (!input) return;
+    setInput("");
+    setItem([...item, input]);
+  };
   return (
     <>
       <input
@@ -11,20 +16,13 @@ function AddMenuInput({ item, setItem }) {
           setInput(e.target.value);
         }}
       />
-      <input
-        type="button"
-        value="提交"
-        onClick={() => {
-          setInput("");
-          setItem([...item, input]);
-        }}
-      />
+      <input type="button" value="提交" onClick={handleClick} />
     </>
   );
 }
 
-function App() {
-  const [item, setItem] = useState([]);
+function Cafe() {
+  const [item, setItem] = useState(["單杯手沖哥倫比雅愛情靈藥", "隨便曼特寧"]);
   return (
     <>
       <h1>八哥狗咖啡廳菜單</h1>
@@ -34,6 +32,30 @@ function App() {
           return <li key={index}>{element}</li>;
         })}
       </ol>
+    </>
+  );
+}
+
+function Restaurant() {
+  const [item, setItem] = useState(["大蝦醋", "鮭魚子"]);
+  return (
+    <>
+      <h1>你才傲嬌日本料理</h1>
+      <AddMenuInput item={item} setItem={setItem} />
+      <ol>
+        {item.map((element, index) => {
+          return <li key={index}>{element}</li>;
+        })}
+      </ol>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <Restaurant />
+      <Cafe />
     </>
   );
 }
