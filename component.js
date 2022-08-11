15b85e0b9287cd1eeecc8611cf1c32a57616685f
Mitour <love2082020@gmail.com>
2022-08-11 11:20:42 +0800

    component: refactoring II

--- component.js
+++ component.js
@@ -14,11 +14,16 @@ function Count({ data }) {
   return <p>總共有{data.length}個產品</p>;
 }
 
+function Modal({ value, meg }) {
+  return <input value={value} type="button" onClick={(e) => alert(meg)} />;
+}
+
 function Product() {
   const productData = ["狗食", "貓食", "狗玩具", "貓玩具"];
   return (
     <>
       <h2>產品頁面</h2>
+      <Modal value="向客戶打招呼" meg="hi~ 妳好嗎？" />
       <Item data={productData} />
       <Count data={productData} />
     </>
@@ -29,6 +34,10 @@ function Cart() {
   return (
     <>
       <h2>購物車頁面</h2>
+      <Modal
+        value="向客戶鼓勵結帳"
+        meg="錢錢沒有不見，只是變成你喜歡的形狀！"
+      />
       <Item data={cartData} />
       <Count data={cartData} />
     </>
