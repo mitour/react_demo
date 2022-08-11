2d6c6e1dc5f6a9de2e711fed574617277f41a6e2
Mitour <love2082020@gmail.com>
2022-08-11 11:27:53 +0800

    useEffect exercise

--- component.js
+++ component.js
@@ -1,56 +1,18 @@
-function Item({ data }) {
-  return (
-    <>
-      <ul>
-        {data.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
-    </>
-  );
-}
+const { useEffect } = React;
 
-function Count({ data }) {
-  return <p>總共有{data.length}個產品</p>;
-}
-
-function Modal({ value, meg }) {
-  return <input value={value} type="button" onClick={(e) => alert(meg)} />;
-}
-
-function Product() {
-  const productData = ["狗食", "貓食", "狗玩具", "貓玩具"];
+function Timer() {
+  const [count, setCount] = useState(0);
+  // 加入 useEffect 練習
+  useEffect(() => {
+    console.log(`click count: ${count}`);
+  }, [count]);
   return (
-    <>
-      <h2>產品頁面</h2>
-      <Modal value="向客戶打招呼" meg="hi~ 妳好嗎？" />
-      <Item data={productData} />
-      <Count data={productData} />
-    </>
+    <div style={{ textAlign: "center" }}>
+      <h1> {count} </h1>
+      <button onClick={() => setCount(count + 1)}>更新</button>
+    </div>
   );
 }
-function Cart() {
-  const cartData = ["狗食", "貓食"];
-  return (
-    <>
-      <h2>購物車頁面</h2>
-      <Modal
-        value="向客戶鼓勵結帳"
-        meg="錢錢沒有不見，只是變成你喜歡的形狀！"
-      />
-      <Item data={cartData} />
-      <Count data={cartData} />
-    </>
-  );
-}
-const App = function () {
-  return (
-    <>
-      <Product />
-      <Cart />
-    </>
-  );
-};
 
 const root = ReactDOM.createRoot(document.getElementById("component"));
-root.render(<App />);
+root.render(<Timer />);
